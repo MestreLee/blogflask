3c8d4d4cf296236593683625ce99d54d41f8bfb8
Mike Tomaco <bernat.cocera@gmail.com>
2022-02-21 17:42:27 +0100

    afegim condicional i estils al formulari

--- hello.py
+++ hello.py
@@ -1,8 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Inicialitzem Flask
 
 app = Flask(__name__)
+
+#Creem la clau secreta pels formularis de flask
+
+app.config['SECRET_KEY']='holaquetalcomoteva'
+
+# Creem un formulari senzill
+
+class FormulariNom(FlaskForm):
+	nom = StringField("Nom: ", validators=[DataRequired()])
+	submit = SubmitField()
 
 @app.route('/')
 def index():
@@ -11,4 +24,13 @@ def index():
 
 @app.route('/about')
 def about():
-	return render_template('about.html', title='About')
+	return render_template ('about.html', title='About')
+
+@app.route('/nom', methods=['GET', 'POST'])
+def nom():
+	form = FormulariNom()
+	nom = None
+	if form.validate_on_submit():
+		nom = form.nom.data
+		form.nom.data = ''
+	return render_template('nom.html', title=nom, nom=nom, form=form)
