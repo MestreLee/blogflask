dd476ac4424749989285223f8a74f60af5cb820f
Mike Tomaco <bernat.cocera@gmail.com>
2022-02-26 21:56:06 +0100

    afegim posts a la db + nou post, posts i post + id

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, url_for, redirect, flash, request
 from flask_wtf import FlaskForm
 from datetime import datetime
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError, TextAreaField
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 #Inicialitzem Flask
@@ -34,7 +35,7 @@ migrate = Migrate(app, db)
 		## 2 - flask db migrate -m 'missatge' (p.e. 'migració inicial')
 		## 3 - flask db upgrade
 
-# Creem el model
+# Creem el model d'usuari
 
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
@@ -44,9 +45,31 @@ class Users(db.Model):
 	# Afegim un camp que no estava d'inici
 	pelicula_preferida = db.Column(db.String(250))
 
+	# Gestions relacionades amb el password (tampoc estava d'inici!)
+	password_hash = db.Column(db.String(128))
+
+	@property
+	def password(self):
+		raise AttributeError('Password no és un atribut que es pugui llegir!')
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
 	# Creem un tostring
 	def __repr__(self):
-		return 'Id: {}, Nom: {}, email: {}, pel.lícula preferida: {}'.format(self.id, self.nom, self.email, self.pelicula_preferida)
+		return 'Id: {}, Nom: {}, email: {}, pel.lícula preferida: {}, password: {}'.format(self.id, self.nom, self.email, self.pelicula_preferida, self.password_hash)
+
+# Creem el model de post
+
+class Posts(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	titol = db.Column(db.String(200), nullable=False)
+	autor = db.Column(db.String(120), nullable=False)
+	data_publicacio = db.Column(db.DateTime, default=datetime.utcnow)
+	contingut = db.Column(db.Text, nullable=False)
 
 # Creem un formulari senzill
 
@@ -54,12 +77,22 @@ class FormulariNom(FlaskForm):
 	nom = StringField("Nom: ", validators=[DataRequired()])
 	submit = SubmitField()
 
-# Creem un formulari per omplir la base de dades
+# Creem un formulari per omplir els usuaris de la base de dades
 
 class FormulariUsuari(FlaskForm):
 	nom = StringField('Nom: ', validators=[DataRequired()])
 	email = StringField('Email: ', validators=[DataRequired()])
 	pelicula_preferida = StringField()
+	password = PasswordField('Password', validators=[DataRequired(), EqualTo('password2')])
+	password2 = PasswordField('Confirm Password', validators=[DataRequired()])
+	submit = SubmitField()
+
+ # Creem un formulari per omplir els posts de la base de dades
+
+class FormulariPost(FlaskForm):
+	titol = StringField('Titol: ', validators=[DataRequired()])
+	autor = StringField('Autor: ', validators=[DataRequired()])
+	contingut = TextAreaField()
 	submit = SubmitField()
 
 @app.route('/')
@@ -87,11 +120,13 @@ def usuari_nou():
 			nom = form.nom.data
 			email = form.email.data
 			pelicula_preferida = form.pelicula_preferida.data
-			user = Users(nom=nom, email=email, pelicula_preferida=pelicula_preferida)
+			#Hash the password
+			hashed_password = generate_password_hash(form.password.data, 'sha256')
+			user = Users(nom=nom, email=email, pelicula_preferida=pelicula_preferida, password_hash=hashed_password)
 			db.session.add(user)
 			db.session.commit()
+			flash('Molt be {} {} {} {}!'.format(nom, email, pelicula_preferida, hashed_password))
 			return redirect(url_for('usuaris'))
-			flash('Molt be {} {} {}!'.format(nom, email, pelicula_preferida))
 		else:
 			flash('Aquest correu ja existeix!')
 	return render_template('usuari_nou.html', title='Usuari Nou', nom=nom, form=form)
@@ -125,14 +160,57 @@ def delete_usuari(id):
 	else:
 		return "Aquest usuari no existeix, capsigrany!"
 
+@app.route('/posts')
+def posts():
+	posts = Posts.query.all()
+	return render_template('posts.html', title='Posts', posts=posts)
+
+@app.route('/posts/nou', methods=['GET', 'POST'])
+def post_nou():
+	form = FormulariPost()
+	titol = None
+	autor = None
+	contingut = None
+	if form.validate_on_submit():
+		titol = form.titol.data
+		autor = form.autor.data
+		contingut = form.contingut.data
+		post = Posts(titol=titol, autor=autor, contingut=contingut)
+		db.session.add(post)
+		db.session.commit()
+		flash('Molt be {} {}!'.format(titol, autor))
+		return redirect(url_for('posts'))
+
+	return render_template('post_nou.html', title='Post Nou', form=form)
+
+@app.route('/posts/<int:id>')
+def post(id):
+	post = Posts.query.get_or_404(id)
+	if post:
+		return render_template('post.html', title='Post', post=post)
+
+@app.route('/posts/<int:id>/update', methods=['GET', 'POST'])
+def update_post(id):
+	post = Posts.query.get_or_404(id)
+	if post:
+		form = FormulariPost()
+		if form.validate_on_submit():
+			post.titol = form.titol.data
+			post.autor = form.autor.data 
+			post.contingut = form.contingut.data
+			db.session.commit()
+			flash('Post creat o modificat {} per: {}!'.format(post.titol, post.autor))
+			return redirect(url_for('posts'))
+		elif request.method == 'GET':
+			form.titol.data = post.titol 
+			form.autor.data = post.autor
+			form.contingut.data = post.contingut
+	return render_template('update_post.html', title='Update post', form=form)
+
 # Tornem els usuaris en format JSON
 
 @app.route('/api')
 def api():
-	# user1 = Users.query.get_or_404(1)
-	# user2 = Users.query.get_or_404(2)
-	# user5 = Users.query.get_or_404(5)
-	# list_users = [user1, user2, user5]
 	llista_users = []
 	llista_users = Users.query.all()
 
